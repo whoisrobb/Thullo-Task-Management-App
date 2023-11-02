fb1858dffb1e7bf2c0526ce5162ecda79af4fdb0
Robbie Muchiri <developedbyrobbie@gmail.com>
2023-11-02 15:06:41 +0300

    deployment settings

--- client/src/utils/urls.js
+++ client/src/utils/urls.js
@@ -1,2 +1,3 @@
 // export const serverUrl = 'http://localhost:4000'
-export const serverUrl = 'https://thullo-task-management-app.vercel.app'
+// export const serverUrl = 'https://thullo-task-management-app.vercel.app'
+export const serverUrl = 'https://thullo-task-management.onrender.com'
